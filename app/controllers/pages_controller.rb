9e1eb5609db25db8966bcfd8a46f10a61f8952ce
Gabriel Mathias <gablemathias@icloud.com>
2022-10-06 21:28:53 -0300

    add form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,5 +9,7 @@ class PagesController < ApplicationController
 
   def contact
     @members = %w[gabriel gustavo mathias dimitri john ted]
+
+    @members = @members.select { |member| member.start_with?(params[:member]) } if params[:member]
   end
 end
